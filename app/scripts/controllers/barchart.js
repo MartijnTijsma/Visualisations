01cfb4fa4bb7c018febba6e60930dd1f6fa7f262
Martijn <martijn@adesys.nl>
2013-11-18 15:17:58 +0100

    renamed files

--- app/scripts/controllers/barchart.js
+++ app/scripts/controllers/barchart.js
@@ -5,6 +5,7 @@ angular.module('visualisationsApp')
     $scope.scoreData = [
         {name: "Greg", score: 98},
         {name: "Ari", score: 96},
+        {name: "Martijn", score: 83},
         {name: 'Q', score: 75},
         {name: "Loser", score: 48}
     ];
